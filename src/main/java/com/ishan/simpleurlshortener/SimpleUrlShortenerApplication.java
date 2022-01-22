be608ed4ab2bf3f3e55d7908b68d6f8e3986e503
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2022-01-22 22:36:58 +0600

    initial commit

---
+++ src/main/java/com/ishan/simpleurlshortener/SimpleUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.simpleurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ishan/simpleurlshortener/SimpleUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.simpleurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
