e764500fccf3b52f828dbaa90e92f54f841c12c1
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2022-01-23 12:07:52 +0600

    url validator check

--- src/main/java/com/ishan/simpleurlshortener/services/DefaultUrlShortenerService.java
+++ src/main/java/com/ishan/simpleurlshortener/services/DefaultUrlShortenerService.java
@@ -5,6 +5,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
@@ -17,16 +20,23 @@ public class DefaultUrlShortenerService implements UrlShortenerService {
 
     @Override
     public String getShortenUrl(String url, HttpServletRequest request) {
+        if(isValidURL(url) == false) {
+            return "Url not valid";
+        }
         String hash = getHash(url);
         String shortUrlHash = hash.substring(0, 7);
         while(shortUrls.containsKey(shortUrlHash)) {
             if(url.equals(shortUrls.get(shortUrlHash))) {
-                return shortUrlHash;
+                return getUrlWithBaseUrl(request, shortUrlHash);
             }
             hash = getHash(hash);
             shortUrlHash = hash.substring(0,7);
         }
         shortUrls.put(shortUrlHash, url);
+        return getUrlWithBaseUrl(request, shortUrlHash);
+    }
+
+    private String getUrlWithBaseUrl(HttpServletRequest request, String shortUrlHash) {
         String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request)
                 .replacePath(null)
                 .build()
@@ -51,5 +61,14 @@ public class DefaultUrlShortenerService implements UrlShortenerService {
         byte[] bytes = md.digest(url.getBytes());
         String hash = DatatypeConverter.printHexBinary(bytes).toUpperCase();
         return hash;
+    }
+
+    private boolean isValidURL(String url) {
+        try {
+            new URL(url).toURI();
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
+        return true;
     }
 }
